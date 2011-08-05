dea71ea57bba31c237dc15035519d1ae12b6fde3
Jan Paulus <jan.paulus@h-brs.de>
2011-08-05 15:00:37 +0200

    added example how to create a data trace

---
+++ examples/create_DataTrace.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <signal.h>
+#include "youbot/YouBotBase.hpp"
+#include "youbot/YouBotManipulator.hpp"
+#include "youbot/DataTrace.hpp"
+
+using namespace std;
+using namespace youbot;
+
+bool running = true;
+
+void sigintHandler(int signal) {
+  running = false;
+  std::cout << std::endl << " Interrupt!" << std::endl;
+}
+
+int main() {
+
+  signal(SIGINT, sigintHandler);
+
+  try {
+
+    YouBotBase myBase("youbot-base");
+
+    myBase.doJointCommutation();
+
+    DataTrace myTrace(myBase.getBaseJoint(4));
+
+    JointCurrentSetpoint currentSetpoint;
+    currentSetpoint.current = 0.4 * ampere;
+
+    myTrace.startTrace();
+
+    while (running) {
+      SLEEP_MILLISEC(1);
+      myBase.getBaseJoint(4).setData(currentSetpoint);
+      myTrace.updateTrace(currentSetpoint);
+    }
+
+    myTrace.stopTrace();
+    myTrace.plotTrace();
+
+    EthercatMaster::getInstance().destroy();
+
+  } catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+  } catch (...) {
+    std::cout << "unhandled exception" << std::endl;
+  }
+  return 0;
+}
