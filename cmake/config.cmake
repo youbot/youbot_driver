04520c5c4e56deff4e8edb764bf4772e85e1efe9
walter <walter@walter-d12.(none)>
2014-01-14 13:49:23 +0100

    fixed install dirs and cmake variables

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -6,5 +6,9 @@
 #    youbot_driver_CONFIG_DIR - Path to config files such as ethercat.cfg
 #
 
-set(youbot_driver_CONFIG_DIR ${youbot_driver_SOURCE_PREFIX}/config)
+if (youbot_driver_SOURCE_PREFIX)
+	set(youbot_driver_CONFIG_DIR ${youbot_driver_SOURCE_PREFIX}/config)
+else (youbot_driver_SOURCE_PREFIX)
+	set(youbot_driver_CONFIG_DIR ${youbot_driver_INSTALL_PREFIX}/share/youbot_driver/config)
+endif (youbot_driver_SOURCE_PREFIX)
 
