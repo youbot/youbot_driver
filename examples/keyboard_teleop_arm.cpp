2a852f92083aa28f5923abb79bc10bf59ea7f316
Praveen Ramanujam <praveen.ramanujam@yahoo.com>
2014-04-01 11:22:48 +0200

    Added keyboard teleop

---
+++ examples/keyboard_teleop_arm.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include "youbot/YouBotBase.hpp"
+#include "youbot/YouBotManipulator.hpp"
+#include "youbot/DataTrace.hpp"
+
+double joint[5];
+double lastJoint[5];
+double jointDelta[5];
+double jointMax[] = {5.840139, 2.617989, -0.0157081, 3.42919, 5.641589};
+double jointMin[] = {0.01006921, 0.01006921, -5.0264, 0.0221391, 0.11062};
+std::vector<double> JointAngles;
+				
+
+
+char getch(void)
+{
+	char ch;
+	struct termios oldt;
+	struct termios newt;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt; 
+	newt.c_lflag &= ~(ICANON | ECHO); 
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt); 
+	ch = getchar(); 
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt); 
+	return ch;
+}
+
+
+int main( int argc, const char* argv[] ){
+	
+	static const int numberOfArmJoints = 5;
+	char keyboardInput = '0';
+	int readValue = 0;
+	JointAngles.resize(5);	
+
+	for(int i = 0; i < numberOfArmJoints; i++)
+	{
+		jointDelta[i] = (jointMax[i] - jointMin[i]) * 0.02;
+	}	
+	
+
+
+	youbot::YouBotManipulator *youBotArm;
+	youBotArm = new youbot::YouBotManipulator("youbot-manipulator");
+	youBotArm->doJointCommutation();
+	youBotArm->calibrateManipulator();
+	std::vector<youbot::JointSensedAngle> jointangles;
+	jointangles.resize(numberOfArmJoints);
+	std::vector<youbot::JointAngleSetpoint> jointSetAngle;
+	jointSetAngle.resize(5);
+	youbot::JointSensedAngle angle;
+	
+
+	while (true) {	
+		keyboardInput = '0';
+		readValue = 0;
+
+		std::cout << "Please provide an arm joint or the gripper to operate on.\n\tType number between 1 and 5, to control joints 1 - 5.\n\tOr hit 6 to go to a preprogrammed position.\n\tOr hit 9 to quit:" << std::endl;
+		std::cin >> readValue;
+		if((readValue >= 1)&&(readValue <= 5))
+		{
+			cout << "Selected arm joint " << readValue << endl;
+		}
+		if(readValue == 9)
+		{
+			cout << "Exiting..." << endl;
+			break;
+		}
+		cout << "Hit w/s to increase/decrease joint angle." << endl;
+		cout << "If you want to stop manipulating this joint, hit 'm' to switch to new joint or exit." << endl;
+		cout << "Hit o/l to open/close gripper." << endl;
+		cout << "To quit, hit Control + C  or 'm'." << endl;
+
+		keyboardInput = '0';
+	
+		while((keyboardInput != 'm'))
+		{	
+			
+			
+			keyboardInput = getch();
+		
+			if (keyboardInput == 'w'){
+
+				for(std::size_t i=0;i<5;i++){
+					youBotArm->getArmJoint(i+1).getData(angle);
+					JointAngles[i] = (double)angle.angle.value();
+				}
+				JointAngles[readValue - 1] += jointDelta[readValue - 1];
+				
+			}
+			if (keyboardInput == 's'){
+						
+				for(std::size_t i=0;i<5;i++){
+					youBotArm->getArmJoint(i+1).getData(angle);
+					JointAngles[i] = (double)angle.angle.value();
+				}
+				JointAngles[readValue - 1] += jointDelta[readValue - 1];
+				
+			}			
+			if(keyboardInput == 'o')
+			{
+				youBotArm->getArmGripper().open();
+			}
+			if(keyboardInput == 'l')
+			{
+				youBotArm->getArmGripper().close();
+			}
+
+			if(keyboardInput != '0')
+			{
+				std::vector<youbot::JointAngleSetpoint> jointSetAngle;
+				jointSetAngle.resize(5);
+				for(std::size_t i=0;i<5;i++){
+					jointSetAngle[i].angle = JointAngles[i]*radian;
+				}
+				youBotArm->setJointData(jointSetAngle);
+				
+			}
+		}
+	}
+	return 0;
+
+}
